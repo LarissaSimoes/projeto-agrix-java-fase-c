1a74b99e36c2a4b368c1b7516042a3f21baba386
Larissa Simões <larissacarneiro2@hotmail.com>
2023-11-06 20:52:14 -0300

    feat: implementa limitação de acesso à rota GET /fertilizers

--- src/main/java/com/betrybe/agrix/controllers/FertilizerController.java
+++ src/main/java/com/betrybe/agrix/controllers/FertilizerController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +36,7 @@ public class FertilizerController {
   }
 
   @GetMapping()
+  @Secured({"ROLE_ADMIN"})
   public List<Fertilizer> findAll() {
     return fertilizerService.findAll();
   }
