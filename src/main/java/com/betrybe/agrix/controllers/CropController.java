099ba9f9aceb0383ddfe8668cd7841c5dcc75a7c
Larissa Simões <larissacarneiro2@hotmail.com>
2023-11-06 20:50:49 -0300

    feat: implementa limitação de acesso à rota GET /farms

--- src/main/java/com/betrybe/agrix/controllers/CropController.java
+++ src/main/java/com/betrybe/agrix/controllers/CropController.java
@@ -13,6 +13,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,7 @@ public class CropController {
    * This method gets all crops.
    */
   @GetMapping
+  @Secured({"ROLE_MANAGER", "ROLE_ADMIN"})
   public ResponseEntity<List<CropDto>> getAllCrops() {
     List<CropDto> crops = cropService.getAllCrops();
     return ResponseEntity.status(HttpStatus.OK).body(crops);
